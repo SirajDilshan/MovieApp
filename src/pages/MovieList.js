970e0b9f96d209d24b96f23bca23d2a6504a09b9
SirajDilshan <sirajdilshan789@gmail.com>
2024-06-14 11:00:19 +0530

    movie list structure

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,17 @@
+import { Card } from "../components";
+
 export const MovieList = () => {
   return (
-    <main>
-      <div className="text-2xl">MovieList</div>
+    <main >
+      <section className="max-w-7xl  ml-20 mr-20 py-7 ">
+        <div className="flex justify-start flex-wrap ">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </section>
     </main>
   );
 };
--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -3,14 +3,18 @@ import { MovieDeatil, MovieList, Search, PageNotFound } from "../pages";
 
 export const AllRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<MovieList />} />
-      <Route path="movie/:id" element={<MovieDeatil />} />
-      <Route path="movies/popular" element={<MovieList />} />
-      <Route path="movies/top" element={<MovieList />} />
-      <Route path="movies/upcoming" element={<MovieList />} />
-      <Route path="search" element={<Search />} />
-      <Route path="*" element={<PageNotFound />} />
-    </Routes>
+    <div className="dark:bg-gray-800">
+      <main>
+        <Routes>
+          <Route path="/" element={<MovieList />} />
+          <Route path="movie/:id" element={<MovieDeatil />} />
+          <Route path="movies/popular" element={<MovieList />} />
+          <Route path="movies/top" element={<MovieList />} />
+          <Route path="movies/upcoming" element={<MovieList />} />
+          <Route path="search" element={<Search />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </main>
+    </div>
   );
 };
