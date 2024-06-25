b79c7944337fca175207b9c5ef88aa12ed6dacf8
SirajDilshan <sirajdilshan789@gmail.com>
2024-06-25 09:11:32 +0530

    end

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -14,8 +14,8 @@ useEffect(() => {
 <main>
 <section className="flex flex-col justify-center px-2" >
   <div className="flex flex-col items-center my-2">
-    <p className="text-7xl text-gray-700 font-bold my-10 dark:text-white" >404, Ooops!</p>
-    <div className="max-w-lg">
+    <p className="text-5xl text-gray-700 font-bold my-10 dark:text-white" >404, Ooops!</p>
+    <div className="max-w-sm">
     <img className="rounded" src={PageNotFoundImage} alt="404 page not found" />
     </div>
     
